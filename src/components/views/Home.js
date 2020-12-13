78a305bb0d3e0f6d9e13d2b92444f841e9f9fe18
Melissa <melissakheying@gmail.com>
2020-12-13 10:03:46 -0600

    :pencil: add about me

--- src/components/views/Home.js
+++ src/components/views/Home.js
@@ -9,18 +9,13 @@ export const Home = () => {
       <div className="col-3 col-s-3 menu">
         <ul>
         <img className='me' src="https://scontent-ort2-1.xx.fbcdn.net/v/t1.0-9/118651673_10218090462076616_9169680802065893598_o.jpg?_nc_cat=102&ccb=2&_nc_sid=174925&_nc_ohc=99lyOcRXiCAAX_qdvxN&_nc_ht=scontent-ort2-1.xx&oh=c87a7c12a831889c15625e4655163272&oe=5FF4E16A" alt="logo" />
-          {/* <li>Microblading</li>
-          <li>Microshading</li>
-          <li>Lips</li>
-          <li>Eyeliner</li>
-          <li>Lash Extensions</li>
-          <li>Hair</li> */}
         </ul>
       </div>
 
       <div className="col-6 col-s-9">
-        <h1>Meet Melissa</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <h1>Meet me, Melissa Heying</h1>
+        <p>What began as a journey of finding myself, I have discovered that I am many things! I a Full-Stack JavaScript developer, Podcast Creator, Nature photographer, GarageBand music maker and much more! I created this site to share all the things that keeps me codetivated. What exactly does codetivated mean? The idea of codetivated came to me after deciding to share my experience of becoming a Web Developer. My intention is to help others realize their potential, specifically in the world of programming but ideally in all aspects of life! Please use my platforms as tools and resources to help you become the Developer you were meant to be! </p>
+        <p>And most importantly, <strong>Stay Codetivated!</strong></p>
       </div>
 
       <div className="col-3 col-s-12">
